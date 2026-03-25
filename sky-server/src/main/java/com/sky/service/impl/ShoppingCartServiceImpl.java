7dacf46c061e2707692e7ab81fc1e89e31ad2251
yangxiaoqi <2200372789@qq.com>
2026-03-25 16:20:29 +0800

    查看购物车代码开发

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -81,4 +81,20 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         //如果不存在，需要插入一条购物车数据
 
     }
+
+    /**
+     * 查看购物车
+     *
+     * @return
+     */
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        // 获取当前微信用户的id
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(userId)
+                .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
+    }
 }
